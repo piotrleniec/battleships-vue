97b1a990b04243800c9f86ef2f3d66270bf4698e
Piotr Leniec <leniec.piotr@gmail.com>
2017-03-10 23:30:41 +0100

    added conditional modifier class applying to cell

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,11 +1,24 @@
 <template>
-  <div class="cell">{{ label }}</div>
+  <div :class="classes">{{ label }}</div>
 </template>
 
 <script>
+import * as cellStates from '@/core/cellStates'
+
 export default {
   name: 'cell',
-  props: ['label']
+  props: ['label', 'state'],
+  computed: {
+    classes: function () {
+      const modifierClass = {
+        [cellStates.FOG_OF_WAR]: 'cell--grey',
+        [cellStates.MISS]: 'cell--blue',
+        [cellStates.HIT]: 'cell--red'
+      }[this.state]
+
+      return ['cell', modifierClass]
+    }
+  }
 }
 </script>
 
