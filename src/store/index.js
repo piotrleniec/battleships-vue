94d90d5b27874322c149425304d8e04e0fff6362
Piotr Leniec <leniec.piotr@gmail.com>
2017-03-11 13:44:17 +0100

    the most basic game module setup

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import game from '@/store/modules/game'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  modules: {}
-})
+export default new Vuex.Store({ modules: { game } })
