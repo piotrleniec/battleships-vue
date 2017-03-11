9c8f8b73f4a8e4883075d5d88649f6f1a52726f2
Piotr Leniec <leniec.piotr@gmail.com>
2017-03-11 12:04:01 +0100

    enemy grid presentation

--- src/core/engines/local/Enemy.js
+++ src/core/engines/local/Enemy.js
@@ -1,7 +1,17 @@
+import { zip } from 'lodash'
 import Grid from '@/core/engines/local/Grid'
+import * as cellTypes from '@/core/cellTypes/enemy'
 
 export default class Enemy {
   constructor () {
     this.grid = Grid.withShips(10)
+  }
+
+  get gridPresentation () {
+    return zip(this.grid.ships, this.grid.hits).map((ship, hit) => {
+      if (!hit) return cellTypes.FOG_OF_WAR
+
+      return ship ? cellTypes.HIT : cellTypes.MISS
+    })
   }
 }
