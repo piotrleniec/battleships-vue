b53561856bdaf4c88181a023a5c511f1b9b8889c
Piotr Leniec <leniec.piotr@gmail.com>
2017-03-11 17:07:55 +0100

    integrated stages with vuex

--- src/store/modules/game/index.js
+++ src/store/modules/game/index.js
@@ -2,10 +2,14 @@ import {
   createGrid, createGridWithShips,
   enemyGridPresentation, playerGridPresentation
 } from '@/store/modules/game/grid'
+import { getStageByName } from '@/core/stages'
+import * as shipsDeployment from '@/core/stages/shipsDeployment'
+import * as types from '@/store/mutation-types'
 
 const state = {
   playerGrid: createGrid(),
-  enemyGrid: createGridWithShips(10)
+  enemyGrid: createGridWithShips(10),
+  currentStageName: shipsDeployment.NAME
 }
 
 const getters = {
@@ -13,8 +17,18 @@ const getters = {
   playerGrid: state => playerGridPresentation(state.playerGrid)
 }
 
-const actions = {}
+const actions = {
+  onCellClick: ({ commit }, x, y) => {
+    commit(types.CURRENT_STAGE_ON_CELL_CLICK, x, y)
+  }
+}
 
-const mutations = {}
+const mutations = {
+  [types.CURRENT_STAGE_ON_CELL_CLICK] (state, x, y) {
+    const currentStage = getStageByName(state.currentStageName)
+
+    currentStage.onCellClick(state, x, y)
+  }
+}
 
 export default { state, getters, actions, mutations }
