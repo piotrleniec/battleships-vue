5512d02c3d288dd6b65f634facd20342d511e629
Piotr Leniec <leniec.piotr@gmail.com>
2017-03-11 18:09:01 +0100

    transition to enemy move stage

--- src/core/stages/playerMove.js
+++ src/core/stages/playerMove.js
@@ -1,5 +1,12 @@
+import Vue from 'vue'
+import * as enemyMoveStage from '@/core/stages/enemyMove'
+
 export const NAME = 'PLAYER_MOVE'
 
 export const onPlayerCellClick = (state, index) => {}
 
-export const onEnemyCellClick = (state, index) => {}
+export const onEnemyCellClick = (state, index) => {
+  Vue.set(state.enemyGrid.hits, index, true)
+
+  state.currentStageName = enemyMoveStage.NAME
+}
