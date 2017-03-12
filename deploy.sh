9e9f1c0ca6468bf78ab8d153891964743d4daa8b
Piotr Leniec <leniec.piotr@gmail.com>
2017-03-12 15:57:12 +0100

    purge asset directory before copying new assets

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ cd ..
 git clone https://github.com/piotrleniec/battleships-vue-preview.git
 cd battleships-vue-preview
 git checkout gh-pages
+rm -rf *
 mv ../dist/* .
 git add -A
 git config user.email 'leniec.piotr@gmail.com'
